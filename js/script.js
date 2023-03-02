3e827277d82c899c5e99d5db3393345ef160fd18
teoremo <teoremo@gmail.com>
2023-03-02 23:33:24 +0000

    adds cal.com link

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*Accardion for 'show more' button begins*/
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,3 +13,14 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+/* Accardion for 'show more' button ends*/
+
+/*Cal element-click embed code begins*/
+
+    (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
+    Cal("init", {origin:"https://app.cal.com"});
+    
+    // Important: Make sure to add `data-cal-link="teoremo/15min"` attribute to the element you want to open Cal on click
+    Cal("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false});
+
+/*Cal element-click embed code ends*/
